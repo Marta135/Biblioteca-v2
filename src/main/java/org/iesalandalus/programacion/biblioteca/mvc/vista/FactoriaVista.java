17ec757c359a95f4539df8d9dff6fc5801818083
marti <marti@192.168.1.211>
2021-03-03 22:25:05 +0100

    12. Creación del enumerado FactoriaVista.

---
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/vista/FactoriaVista.java
@@ -0,0 +1,18 @@
+package org.iesalandalus.programacion.biblioteca.mvc.vista;
+
+import org.iesalandalus.programacion.biblioteca.mvc.vista.texto.VistaTexto;
+
+public enum FactoriaVista {
+
+	TEXTO {
+		public IVista crear() {
+			return new VistaTexto();
+		}
+	};
+	
+	FactoriaVista(){
+		
+	};
+	
+	public abstract IVista crear();
+}
